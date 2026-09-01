8a9167ec9fd6af2d77d0817c0754aa1c7e4d144f
Santiago Reyes <santiagoreyes@Santiagos-MacBook-Air.local>
2026-09-01 12:52:26 -0400

    fianl presentation

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 
 const BrowseByCategory = () => {
   return (
-    <section id="section-category" className="no-top">
+    <section
+  id="section-category"
+  className="no-top"
+  data-aos="fade-left"
+  data-aos-duration="1000"
+>
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
